d34ef4a9014dbceb57b15fa087eb6692d0013ca4
mfabianchm <mfchongm@hotmail.com>
2024-02-26 10:09:48 -0600

    Initial Commit

---
+++ age-calculator-app/age_calculator_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  age_calculator_appApp.swift
+//  age-calculator-app
+//
+//  Created by Marcos Fabian Chong Megchun on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct age_calculator_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
